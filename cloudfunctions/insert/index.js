a9d4bea2f59e28b6c3ff63a1aca3a4bc8771b73b
iamzhaoxin <zhaoxin130120@126.com>
2021-03-28 15:38:06 +0800

    简单实现：输入数据->发送到云函数->储存到云数据库->回调函数->显示返回的值

--- cloudfunctions/insert/index.js
+++ cloudfunctions/insert/index.js
@@ -1,11 +1,24 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
-
+cloud.init({
+  //要指定环境，不然默认使用第一个创建的环境，那就糟糕了……
+  env: 'mini-program-8gduw4z9cd88bd53',
+})
+const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
-  return {
-    sum: event.a+event.b
+  try {
+    await db.collection('test').add({
+      data:{
+        money: event.money,
+      }
+    })
+    return "okkk"
+  } catch (e) {
+    console.log(e)
   }
+  // return {
+  //   sum: parseFloat(event.money)*2
+  // }
 }
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,17 +1,31 @@
-
 Page({
-  addDate:function(){
-      wx.cloud.callFunction({
-        name:'insert',
-        data:{
-          a:3,
-          b:4,
-        },
-        success: function(res) {
-          console.log(res.result.sum) 
-        },
-        fail: console.error
-      })
+  //设置初始化时要渲染的数据
+  data: {
+    money: '',
+    output: '',
+  },
+  //获取input的值（相应bindinput='get_money'）
+  get_money(e) {
+    this.setData({
+      money: e.detail.value,
+    })
+  },
+  //调用云函数
+  addData: function () {
+    var that = this;
+    wx.cloud.callFunction({
+      name: 'insert',
+      data: {
+        money: this.data.money,
+      },
+      success: function (res) {
+        console.log(res.result)
+        that.setData({
+          output: res.result
+        })
+      },
+      fail: console.error
+    })
   }
 
 })
